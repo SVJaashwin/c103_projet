8b3ab6cc64dd06aa52870c3acdffcf49cd90a2d2
SVJaashwin <88593780+SVJaashwin@users.noreply.github.com>
2022-01-28 13:20:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//https://teachablemachine.withgoogle.com/models/yxfJb1O9A/
+//https://teachablemachine.withgoogle.com/models/Kl91i83n1/
 
 
 Webcam.set({width:350,height:300,img_format:"png",png_quality:90});
@@ -14,7 +14,22 @@ function camera_on(){
 });
 }
 console.log('ml5 version:',ml5.version);
-classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/yxfJb1O9A/model.json",modelLoaded);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Kl91i83n1/model.json",modelLoaded);
 function modelLoaded() {
       console.log("model loaded");
+}
+function check (){
+    var captured = document.getElementById("captured_img") ;
+    classifier.classify(captured,getResult) ;
+
+}
+function getResult (error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_accuracy").innerHTML = results[0].confidence.toFixed(4);
+    }
 }
